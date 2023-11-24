6f5204ff33b06c9ba4bc341fad94c55f5e3314ef
unknown <hamidouboum@hotmail.fr>
2023-11-24 15:01:04 +0100

    UPDATE: updated the error 404 page.

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,5 +1,9 @@
 function Page404() {
-  return <div>oups, la page demandée n'existe pas</div>;
+  return (
+    <div className="error">
+      <h1>Ho HO HO!! Y a rien ici</h1>
+    </div>
+  );
 }
 
 export default Page404;
